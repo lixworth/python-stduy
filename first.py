8d5c4aaa1a4384ff35c4f2af8c75d9241ece43ce
LixWorth <lixworth@gmail.com>
2020-05-07 08:50:56 +0800

    5/6

--- first.py
+++ first.py
@@ -2,3 +2,19 @@
 # @author LixWorth
 
 print("Bug dhdj !")
+print("dhdj","is","a","bug")
+
+print(100+100*2-1/2*6)
+
+# name = input()
+# print("bug :",name)
+print("\\")
+print("bu\tg\ndhdj")
+print('''bugdhdj
+... yes
+... oh ''')
+
+dhdj = "bug"
+bug = "dhdj"
+
+print(dhdj,bug)
